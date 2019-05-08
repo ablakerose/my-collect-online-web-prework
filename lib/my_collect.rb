2a33282bbb360f9ff60f13aab96ff5f897f3e73b
Blake Rose <bdecktor@gmail.com>
2019-05-08 11:52:12 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+require "pry"
+def my_collect(empty_array)
+  my_collect_capitalized = []
+  empty_array.each |name|
+  name.upcase
+  end
+  name = my_collect_capitalized
+  binding.pry
+  end
+  my_collect_capitalized
+end
